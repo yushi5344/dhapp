b19f324fbc505109b504302ebfdafa7020519c58
guomin <yushi5344@gmail.com>
2018-07-28 13:56:06 +0800

    登录及退出

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+ 	server: 'http://119.3.26.220/ranzheng_donghui_new/api.php',
+ 	token:'608e870bde43bbb273807f5bee766f8f',
+	apiname: 'topapi', //系统配置的api名称
+	pagesize: 10, // 分页组件每页显示数量
+	cpage: 1, //分页当前页
+	apimethod: { //接口method集合
+		logout: 'Login.loginOut', //退出
+		login:'Login.loginIn',//登录
+	}
+}
---
+++ js/app.js
@@ -0,0 +1,61 @@
+var login=function(loginInfo, callback){
+	callback = callback || mui.noop;
+	loginInfo = loginInfo || {};
+	loginInfo.account = loginInfo.account || '';
+	loginInfo.password = loginInfo.password || '';
+	if (loginInfo.account.length =='') {
+		return callback('账号不能为空');
+	}
+	if (loginInfo.password.length < 8) {
+		return callback('密码最短为 8 个字符');
+	}
+	//开始登录
+	var arr={'userName':loginInfo.account,'passwd':loginInfo.password,'token':config.token};
+	var data=request('POST',arr,config.apimethod.login);
+	mui.toast('登录成功');
+	setTimeout(function(){
+		return createState(loginInfo.account, callback);
+	},600);
+	
+};
+
+var logout=function(){
+	var arr={'token':config.token};
+	var data=request('POST',arr,config.apimethod.logout);
+}
+var request=function(type,params,method){
+	mui.ajax({
+		url:config.server,
+		type:type,
+		dataType:'json',
+		data:{
+			params:params,
+			method:method
+		},
+		success:function(data){
+			console.log(JSON.stringify(data));
+			if(data.success==false){
+				mui.alert(data.msg);
+			}else{
+				return data;
+			}
+		},
+		error:function(xhr,type,errorThrown){
+			console.log(xhr);
+		}
+	});
+}
+var createState = function(name, callback) {
+	var state = getState();
+	state.account = name;
+	setState(state);
+	return callback();
+};
+var getState = function() {
+	var stateText = localStorage.getItem('$state') || "{}";
+	return JSON.parse(stateText);
+};
+var setState = function(state) {
+	state = state || {};
+	localStorage.setItem('$state', JSON.stringify(state));
+};
